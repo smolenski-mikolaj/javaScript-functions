acaaae8ab7b5e94dc53d484bae2b2a2e3e2bfece
smolenski-mikolaj <31181850+smolenski-mikolaj@users.noreply.github.com>
2017-08-27 11:57:44 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,3 @@
 var a = 3,
 	b = 2,
 	value;
-
-value = (a * a) + (2 * a * b) - (b * b);
-console.log('Wynik: ' + value);
-
-if (value > 0) {
-	console.log('Wynik dodatni');
-} else if (value < 0) {
-	console.log('Wynik ujemny');
-} else if (value == 0) {
-	console.log('Wynik równy 0');
-}
-
