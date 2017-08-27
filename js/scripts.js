0159de769ce82bb49f9c10a49d7963552587e2d6
smolenski-mikolaj <31181850+smolenski-mikolaj@users.noreply.github.com>
2017-08-27 12:06:24 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,17 @@ var a = 3,
 	b = 2,
 	value;
 
-value = (a * a) + (2 * a * b) - (b * b);
-console.log('Wynik: ' + value);
-
-if (value > 0) {
-	console.log('Wynik dodatni');
-} else if (value < 0) {
-	console.log('Wynik ujemny');
-} else if (value == 0) {
-	console.log('Wynik równy 0');
+if ( isNaN(a) == false &&  isNaN(b) == false ) {
+	
+	value = (a * a) + (2 * a * b) - (b * b);
+	console.log('Wynik: ' + value);
+	
+	if (value > 0) {
+		console.log('Wynik dodatni');
+	} else if (value < 0) {
+		console.log('Wynik ujemny');
+	} else if (value == 0) {
+		console.log('Wynik równy 0');
+	}
 }
+
